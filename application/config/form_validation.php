a797d94b6036a417e33370a7c9a457572a0e7565
celine31 <dadou20231@gmail.com>
2018-04-26 13:06:03 +0200

    vérification du formulaire, ajout de la page présentation et de la confirmation d'email

--- application/config/form_validation.php
+++ application/config/form_validation.php
@@ -6,9 +6,28 @@ $config =array(
         array(
             'field'=>'name',
             'label'=>'nom',
+            'rules'=>'required|min_length[5]|max_length[12]'
+        ),
+        array(
+            'field'=>'email',
+            'label'=>'email',
+            'rules'=>array('valid_email'=>'required')
+        ),
+        array(
+            'field'=>'confEmail',
+            'label'=>"confirmation d'email",
+            'rules'=>array('valid_email','required','matches[email]')
+        ),
+        array(
+            'field'=>'title',
+            'label'=>'title',
             'rules'=>'required'
         ),
-        
+        array(
+            'field'=>'message',
+            'label'=>'message',
+            'rules'=>'required'
+        )
     )
-)
+);
 
